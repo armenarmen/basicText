07a9d205a890d0a7ef45bd6607c7841fabb07300
Armen <armen@Armens-MacBook-Pro.local>
2015-06-09 17:08:37 -0700

    added entries, and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
+  resources :entries
+
+  resources :users do
+    resources :entries
+  end
+  get 'signup'  => 'users#new'
+
   root to: 'visitors#index'
 end
