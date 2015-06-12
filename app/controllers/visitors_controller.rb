d93c13a13bfd94ed6f0e2200d59d65d2aa0c59a1
Armen <armen@Armens-MacBook-Pro.local>
2015-06-12 01:46:38 -0700

    solves log in issues

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,12 +1,23 @@
 class VisitorsController < ApplicationController
   include SessionsHelper
   def index
-    @user = @current_user || User.new(email: "#{(0...8).map { (65 + rand(26)).chr }.join}@example.com",
+    if logged_in?
+      puts "logged_in = true ----------------"
+      @user = @current_user
+    else
+       @user = User.new(email: "#{(0...8).map { (65 + rand(26)).chr }.join}@example.com",
                                       password: "example1",
                                       password_confirmation: "example1")
-    @user.save!
-    # log_in @user # this is probably bad
+       @user.save!
+
+    end
+    # @user = @current_user || User.new(email: "#{(0...8).map { (65 + rand(26)).chr }.join}@example.com",
+    #                                   password: "example1",
+    #                                   password_confirmation: "example1")
+    
+    puts ">>>>>>>>>>>>>>>>>>>>>>> logged in ? -> #{logged_in?} id = #{@user.id}"
+    log_in @user # this is probably bad
     @entry = @user.entries.build
-    puts ">>>>>>>>>> #{@user.id}"
+    # puts ">>>>>>>>>> "
   end
 end
