809baec23ea052a361ae40189f6f26a35cd027ed
Armen <armen@Armens-MacBook-Pro.local>
2015-06-12 15:14:54 -0700

    partial mailer logic

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,12 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 
+  def entries_email(user)
+    @user = user
+    @entries = @user.entries.group_by { |stuff| stuff.created_at.to_date }[Date.yesterday]
+    # Entry.where(user_id: 104).group_by { |stuff| stuff.created_at.to_date }
+    # if entries != 0; send the mail.
+    # build the fucking views, loop over. 
+  end
+
 end
