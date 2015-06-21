17f89591452715f3152c73724e71be00ee11ac8c
Armen <armen@Armens-MacBook-Pro.local>
2015-06-21 16:55:45 -0700

    better logging, fixed query in emailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,7 +10,7 @@ class UserMailer < ApplicationMailer
   def entries_email(user)
     puts ">>>>>>>>>>>>>>>>>>>> entries email >>>>>>>"
     @user = user
-    @entries = @user.where("DATE(created_at) = ?", Time.now-1.day)
+    @entries = @user.entries.where("DATE(created_at) = ?", Time.now-1.day)
     if @entries.present?
       puts "there were some here"
       mail(to: @user.email, subject: 'Your Writings from yesterday!')
--- lib/tasks/entries_email.rake
+++ lib/tasks/entries_email.rake
@@ -1,7 +1,7 @@
 task :send_entries => :environment do
   desc "Gets users that have been updated, ie are 'real'"
   users = User.where("created_at <> updated_at")
-  puts "task called"
+  puts "task called #{users.count}"
   users.each do |a|
     puts "#{a.name}: #{a.email}"
     UserMailer.entries_email(a)
