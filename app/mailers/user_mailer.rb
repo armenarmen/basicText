7be140c2230d5909c5f35ec3e14fb0117478d8a7
Armen <armen@Armens-MacBook-Pro.local>
2015-06-21 16:47:31 -0700

    cleaned up emailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,20 +7,10 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 
-  # def entries_email(user)
-  #   @user = user
-  #   @entries = @user.entries.group_by { |stuff| stuff.created_at.to_date }[Date.yesterday]
-  #   if @entries.present?
-  #     mail(to: @user.email, subject: 'Your Writings from yesterday!')
-  #   end
-  #   # Entry.where(user_id: 104).group_by { |stuff| stuff.created_at.to_date }
-  #   # if entries != 0; send the mail.
-  #   # build the fucking views, loop over. 
-  # end
   def entries_email(user)
     puts ">>>>>>>>>>>>>>>>>>>> entries email >>>>>>>"
     @user = user
-    @entries = @user.entries.group_by { |stuff| stuff.created_at.to_date }[Date.yesterday]
+    @entries = @user.where("DATE(created_at) = ?", Time.now-1.day)
     if @entries.present?
       puts "there were some here"
       mail(to: @user.email, subject: 'Your Writings from yesterday!')
